bf8a2a6664b77975d8db56b5b12607778fb552b6
masrul <mail2masrul@gmail.com>
2020-05-22 21:14:06 -0500

    moveTo/by

--- MolAligner/molaligner.py
+++ MolAligner/molaligner.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import math 
 
 class Aligner:
     
@@ -54,9 +55,9 @@ class Aligner:
             ]
         coords=do_align(vec1,target_dir,coords)  
         
-        self._x = coords[0,:]
-        self._y = coords[1,:]
-        self._z = coords[2,:] 
+        self._x = list(coords[0,:])
+        self._y = list(coords[1,:])
+        self._z = list(coords[2,:])  
     
 
 
@@ -80,16 +81,39 @@ class Aligner:
         
         filehandler.close() 
     
-    def translate(self,transPos): 
+    def moveTo(self,transPos,atomID = None): 
+
+        '''
+        tranPos: coordinate of target point, where COM or atomID will
+         be moved  
+        '''
         
-        xcom = sum(self._x)/self._natoms 
-        ycom = sum(self._y)/self._natoms 
-        zcom = sum(self._z)/self._natoms 
+        if  atomID: 
+            xref = self.x[atomID-1] 
+            yref = self.y[atomID-1] 
+            zref = self.z[atomID-1] 
+        else: 
+            xcom = sum(self._x)/self._natoms 
+            ycom = sum(self._y)/self._natoms 
+            zcom = sum(self._z)/self._natoms 
+            
+            xref = xcom; yref = ycom; zref = zcom 
+
+
 
         for i in range(self._natoms): 
-            self._x[i] = self._x[i] - xcom + transPos[0]
-            self._y[i] = self._y[i] - ycom + transPos[1]
-            self._z[i] = self._z[i] - zcom + transPos[2]
+            self._x[i] = self._x[i] - xref + transPos[0]
+            self._y[i] = self._y[i] - yref + transPos[1]
+            self._z[i] = self._z[i] - zref + transPos[2] 
+
+
+    def moveBy(self, transVector): 
+        
+        for i in range(self.natoms): 
+            self.x[i] += transVector[0] 
+            self.y[i] += transVector[1] 
+            self.z[i] += transVector[2] 
+
 
     def findMoleculeAxis(self):  
 
@@ -108,6 +132,18 @@ class Aligner:
         argmax = np.argmax(dist) 
 
         return pairs[argmax][0] + 1, pairs[argmax][1] + 1 
+    
+    
+    def merge(self,other): 
+        
+        self._natoms += other._natoms  
+        self._symbols += other._symbols 
+        self._resids  += [resid+self._resids[-1]  for resid  in other._resids]  
+        self._atomids += [atomid+self._atomids[-1]  for atomid  in other._atomids] 
+        self._resnames += other._resnames 
+        self._x += other._x 
+        self._y += other._y 
+        self._z += other._z 
     
     @property 
     def x(self): 
@@ -137,9 +173,13 @@ class Aligner:
     def atomids(self): 
         return self._atomids 
 
-def get_rot_matrix(v,c): 
+def getAlignMatrix(v,c): 
 
-    rotMat=np.zeros((3,3),dtype=float)
+    '''
+    https://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d 
+    '''
+
+    alignMat=np.zeros((3,3),dtype=float)
     iMat=np.zeros((3,3),dtype=float)
     vx=np.zeros((3,3),dtype=float)
 
@@ -161,9 +201,46 @@ def get_rot_matrix(v,c):
 
     factor=1.0/(1.0+c)
 
-    rotMat=iMat + vx + np.matmul(vx,vx) * factor 
+    alignMat=iMat + vx + np.matmul(vx,vx) * factor 
+    
+    return alignMat 
+
+def getRotMatrix(rotAxis,angle): 
+    rotMat=np.zeros((3,3),dtype=float)
+    
+    angle = angle * 0.0174533  # degree to radian 
+
+    cos = math.cosine(angle)
+    sin = math.sine(anlge) 
+    _cos_ = 1 - cos
+    
+    u = rotAxis / np.linalg.norm(u) 
+    
+    ux = u[0]; uy = u[1]; uz = u[2] 
+
+    uxy = ux * uy 
+    uyz = uy * uz 
+    uzx = uz * ux 
+    uxx = ux * ux 
+    uyy = uy * uy
+    uzz = uz * uz 
+
+    rotMat[0,0] = cos + (uxx * _cos_) 
+    rotMat[0,1] = (uxy * _cos_) - (uz * sin) 
+    rotMat[0,2] = (uzx * _cos_) + (uy * sin)
+
+    rotMat[1,0] = (uxy * _cos_) + (uz * sin) 
+    rotMat[1,1] = cos + (uyy * _cos_) 
+    rotMat[1,2] = (uyz * _cos_) - (ux * sin) 
+
+    rotMat[2,0] = (uzx * _cos_) - (uy * sin) 
+    rotMat[2,1] = (uyz * _cos_) + (ux * sin)  
+    rotMat[2,2] = cos + (uzz*_cos_) 
+
     
     return rotMat 
+
+
 
 def do_align(u,v,coords): 
     
@@ -171,10 +248,28 @@ def do_align(u,v,coords):
     v=v/np.linalg.norm(v) 
 
     normal=np.cross(u,v)
-    c=np.dot(u,v)
+    c=np.dot(u,v) 
+
+    if abs(abs(c)-1)<10e-10:  # if vectors are antiparallel 
+        coords= coords * -1 
+        return coords 
+
     
-    rotMat=get_rot_matrix(normal,c)
-    coords=np.matmul(rotMat,coords) 
+    alignMat=getAlignMatrix(normal,c)
+    coords=np.matmul(alignMat,coords) 
     
     return(coords)
+
+
+# m0 =Aligner('hemcel.pdb') 
+# m0.align(5,294,[0,0,1]) 
+# m0.moveTo([10,0,0]) 
+# # molecule0.write('out0.pdb') 
+
+# m1 = Aligner('cellulose_2.pdb') 
+# m1.align(2,246,[0,0,1]) 
+# m1.moveTo([0,0,0]) 
+
+# m0.merge(m1)
+# m0.write('out-merged.pdb') 
 
